72b34e8a32b275bc0b7e189d6213327f8bb76cd6
MateoVillella <mateo.villella@sarsy-srl.com>
2024-09-18 18:22:49 -0300

    Se añade la nueva activity

--- app/src/main/java/com/example/tp3_grupo2/MainActivity.java
+++ app/src/main/java/com/example/tp3_grupo2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.tp3_grupo2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void redireccionRegistrarse(View view){
+        Intent intent= new Intent(this, Registrarse.class);
+        startActivity(intent);
     }
 }
